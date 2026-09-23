5f992533c7914f0525e299a64dd3bc46e692db2f
ColinG05 <colinjgilbert2005@gmail.com>
2026-09-23 16:59:07 -0400

    Create MedianTwoSortedArrays.py

---
+++ MedianTwoSortedArrays.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums = sorted(nums1 + nums2) 
+        length = len(nums)
+        mid = length // 2
+
+        if length %2: 
+            return nums[mid]
+        else:
+             return (nums[mid-1] + nums[mid]) / 2.0
+
+        
+        
