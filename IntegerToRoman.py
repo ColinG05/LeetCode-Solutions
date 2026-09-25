d15d3844cbf3c8c39018ccb142c0db9117aef284
ColinG05 <colinjgilbert2005@gmail.com>
2026-09-25 13:20:17 -0400

    Create IntegerToRoman.py

---
+++ IntegerToRoman.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+
+        #okay this is my thinking here, given some number say 15, this result would be XV right, so we can think of this as reading character by character say  
+
+        value_symbols = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+        result = ""
+
+        for value, symbol in value_symbols: 
+                if num ==0:
+                    break
+                count = num//value
+                result += (symbol*count)
+                num-= count*value
+        return result
+        
